a8f0ae74375b76b8dc12a6c46a02bf74aa22cdf0
Sameer rai <sameerrai350@gmail.com>
2024-02-12 23:54:55 +0530

    update the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,15 +14,15 @@ const Footer = () => {
     return (
 
         <Flex borderTop={'1px solid purple'} color={'#fff'} minH={'30vh'} bgColor={'gray.900'} p={'8'} flexDirection={['column', 'row']} justifyContent={'center'} alignItems={'center'}>
-            <Box mx={'6'} maxW={'20vw'}>
-                <Heading fontWeight={'400'} children={'Flying_Monk'}/>
+            <HStack flexDir={'column'} mx={'6'} maxW={'20vw'}>
+                <Heading fontWeight={'400'} textAlign={'center'} w={'fit-content'} children={'Flying_Monk'}/>
                 <Text>All Rights Reserved</Text>
-            </Box>
-            <Box mx={'6'} flex="1">
+            </HStack>
+            <HStack flexDir={'column'} mx={'6'} flex="1">
                 <Image src={img} alt="Logo" maxW={{ base: "100px", md: "120px" }} mb={4} />
                 <Text fontSize="sm">Our vision is to make all people the best place to live for them</Text>
-            </Box>
-            <Box mx={'6'} flex="1">
+            </HStack>
+            <HStack flexDir={'column'} mx={'6'} flex="1">
                 <Heading size="md" mb={4}>Information</Heading>
                 <Text>145 New York, FL 5497, USA</Text>
                 <HStack mt={2}>
@@ -33,8 +33,8 @@ const Footer = () => {
                     
                     
                 </HStack>
-            </Box>
-            <Button pos={'absolute'} bottom={'2'} right={'2'} background={'transparent'} color={'#fff'} onClick={scrollToTop}><FaArrowAltCircleUp fontSize={'25px'} /></Button>
+            </HStack>
+            <Button pos={'absolute'} bottom={['400','2']} right={'2'} background={'transparent'} color={'#fff'} onClick={scrollToTop}><FaArrowAltCircleUp fontSize={'25px'} /></Button>
         </Flex>
     )
 }
